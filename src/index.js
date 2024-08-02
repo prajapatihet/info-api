a0dd7aa770434c6ba18a7d39ea458683c0b79a8f
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-08-02 12:48:28 +0530

    donorlist-updated

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-// src/index.js
 addEventListener('fetch', event => {
     event.respondWith(handleRequest(event.request));
 })
 
 async function handleRequest(request) {
-    // Fetch data from the given URL
+    const url = new URL(request.url);
+
+    switch (url.pathname) {
+        case '/bloodbank':
+            return handleBloodbankRequest();
+        case '/hospital':
+            return handleDonorRequest();
+        default:
+            return new Response('Not Found', { status: 404 });
+    }
+}
+
+async function handleBloodbankRequest() {
     const response = await fetch('https://prajapatihet.github.io/hospitalinfo-api/bloodbank.json', {
         method: 'GET',
         headers: {
@@ -12,10 +23,21 @@ async function handleRequest(request) {
         }
     });
 
-    // Retrieve the data from the response
     const data = await response.json();
+    return new Response(JSON.stringify(data), {
+        headers: { 'Content-Type': 'application/json' }
+    });
+}
 
-    // Return the data as a new response
+async function handleDonorRequest() {
+    const response = await fetch('https://prajapatihet.github.io/hospitalinfo-api/donorinfo.json', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const data = await response.json();
     return new Response(JSON.stringify(data), {
         headers: { 'Content-Type': 'application/json' }
     });
